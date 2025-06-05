c1925f73bd32833d787b7b589c10fff301f3cbb8
G05th <germanochita27@gmail.com>
2025-06-06 00:59:44 +0100

    sabor Luanda

--- react/react-restaurant/vite.config.ts
+++ react/react-restaurant/vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/portfolio/react/react-restaurant/',
   plugins: [react()],
   resolve: {
     alias: {
